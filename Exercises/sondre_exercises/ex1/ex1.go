287a387365223d19ccd39ae90638ec55934dde76
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-01-11 13:55:33 +0100

    Update Sondre's exercise files

--- Exercises/sondre_exercises/ex1/ex1.go
+++ Exercises/sondre_exercises/ex1/ex1.go
@@ -9,17 +9,28 @@ import (
     "time"
 )
 
-func someGoroutine() {
-    Println("Hello from a goroutine!")
+var num int
+
+// Are all functions go routines?
+func threadIncNum() {
+    for i := 0; i < 1000000; i++ {
+        num++
+    }
+}
+
+func threadDecNum() {
+    for i := 0; i < 1000000; i++ {
+        num--
+    }
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
                                             // Try doing the exercise both with and without it!
-    go someGoroutine()                      // This spawns someGoroutine() as a goroutine
-
+    go threadIncNum()                      // This spawns threadIncNum() as a goroutine
+    go threadDecNum()
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
-    Println("Hello from main!")
+    Println("Num: ", num)
 }
