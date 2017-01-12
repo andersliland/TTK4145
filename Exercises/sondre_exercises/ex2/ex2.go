b0e3f52d0743b53787e75581c62f664f0c9f29ae
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-01-12 16:19:48 +0100

    Use gofmt for standard Go formatting of code

--- Exercises/sondre_exercises/ex2/ex2.go
+++ Exercises/sondre_exercises/ex2/ex2.go
@@ -4,54 +4,56 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
+	. "fmt"
+	"runtime"
 )
 
-func server(ch1, ch2, finished chan bool) int {    
-    num := 0
-    exit := 0
-    for {
-        select {
-            case <-ch1:
-                num++
-            case <-ch2:
-                num--
-            case <-finished:
-                exit++
-                if exit == 2 {
-                    return num
-                }
-        }
-    }
+func server(ch1, ch2, finished chan bool) int {
+	num := 0
+	exit := 0
+	for {
+		select {
+		case <-ch1:
+			num++
+			Println(num)
+		case <-ch2:
+			num--
+			Println(num)
+		case <-finished:
+			exit++
+			if exit == 2 {
+				return num
+			}
+		}
+	}
 }
 
 // Are all functions Go routines?
 func threadIncNum(ch1, finished chan bool) {
-    for i := 0; i < 10; i++ {
-        ch1 <- true
-    }
-    finished <- true
+	for i := 0; i < 10; i++ {
+		ch1 <- true
+	}
+	finished <- true
 }
 
 func threadDecNum(ch2, finished chan bool) {
-    for i := 0; i < 10; i++ {
-        ch2 <- true
-    }
-    finished <- true
+	for i := 0; i < 10; i++ {
+		ch2 <- true
+	}
+	finished <- true
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    
-    ch1 := make(chan bool)
-    ch2 := make(chan bool)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-    finished := make(chan bool, 1)
+	ch1 := make(chan bool, 1)
+	ch2 := make(chan bool, 1)
 
-    go threadIncNum(ch1, finished)
-    go threadDecNum(ch2, finished)
-    num := server(ch1, ch2, finished)
+	finished := make(chan bool, 1)
 
-    Println("Num: ", num)
+	go threadIncNum(ch1, finished)
+	go threadDecNum(ch2, finished)
+	num := server(ch1, ch2, finished)
+
+	Println("Num: ", num)
 }
